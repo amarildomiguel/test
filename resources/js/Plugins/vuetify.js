c8a32f4f27c98fabe42ea2469f873bc35beed594
Amarildo Miguel <amarildomgl@gmail.com>
2024-09-27 09:54:20 +0100

    feat: add vuetify styles

--- resources/js/Plugins/vuetify.js
+++ resources/js/Plugins/vuetify.js
@@ -3,6 +3,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import colors from 'vuetify/util/colors'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
@@ -15,6 +16,22 @@ const vuetify = createVuetify({
     aliases,
     sets: {
       mdi,
+    },
+  },
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: colors.teal.darken4,
+          accent: colors.teal.lighten1,
+          secondary: colors.teal.lighten4,
+          error: colors.red.darken2,
+          warning: colors.amber.darken2,
+          info: colors.blue.darken2,
+          success: colors.green.darken2,
+        },
+      },
     },
   },
 })
